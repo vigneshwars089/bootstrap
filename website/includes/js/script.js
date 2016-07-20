96d1a6f8d3813194082748919708138998797514
vigneshwars089 <vign12320.it@rmkec.ac.in>
2016-07-20 09:48:15 +0530

    Add files via upload

---
+++ website/includes/js/script.js
@@ -0,0 +1,13 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
